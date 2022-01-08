871c4b7c42579c202911c442adce055804b12526
ogMav8 <brad.anderson019@gmail.com>
2022-01-07 21:54:15 -0700

    file: nav.jsx - changed to section and div layout

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -16,35 +16,37 @@ const Nav = () => {
     // <nav id="navbar" className="navbar active fixed-top navbar-expand-lg navbar-light">
 
     return (
-        <nav id="navbar" className={navbar ? 'navbar active fixed-top navbar-expand-lg navbar-dark' : 'navbar fixed-top navbar-expand-lg navbar-light'}>
-            <div className="container">
-            <a className={navbar ? 'navbar-brand' : "navbar-brand-top"}  href="#">Bradford Anderson</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#about">About me</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#contact">Contact</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#portfolio">Portfolio</a>
-                    </li>
-                </ul>
-                <div className="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        RESUME
-                    </button>
-                    <div className="dropdown-menu pl-2" aria-labelledby="dropdownMenuButton">
-                        <a href={Anderson_Bradford_Resume} download>Resume</a>
+        <section id="navbar">
+            <div className={navbar ? 'navbar active fixed-top navbar-expand-lg navbar-dark' : 'navbar fixed-top navbar-expand-lg navbar-light'}>
+                <div className="container">
+                <a className={navbar ? 'navbar-brand' : "navbar-brand-top"}  href="#">Bradford Anderson</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#about">About me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#portfolio">Portfolio</a>
+                        </li>
+                    </ul>
+                    <div className="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            RESUME
+                        </button>
+                        <div className="dropdown-menu pl-2" aria-labelledby="dropdownMenuButton">
+                            <a href={Anderson_Bradford_Resume} download>Resume</a>
+                        </div>
                     </div>
                 </div>
+                </div>
             </div>
-            </div>
-        </nav>
+        </section>
 
     )
 }
